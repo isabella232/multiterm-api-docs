4b6ebc4eae31ae31ca9ab4dea422808f41504635
ituriSdl <ituri@sdl.com>
2021-09-03 13:30:10 +0200

    Updated repo name

--- releaseDocs.ps1
+++ releaseDocs.ps1
@@ -9,7 +9,7 @@ write-host  "`ACTOR is $ACTOR"
 $SOURCE_DIR=$psscriptroot
 $TEMP_REPO_DIR=[System.IO.Path]::GetFullPath("$psscriptroot/../docs-gh-pages")
 
-$remote_repo="https://github-actions:${TOKEN}@github.com/sdl/multiterm-api-docs.git"
+$remote_repo="https://github-actions:${TOKEN}@github.com/rws/multiterm-api-docs.git"
 
 write-host "Removing temporary doc directory $TEMP_REPO_DIR"
 Remove-Item $TEMP_REPO_DIR -Force -Recurse -ErrorAction Ignore
--- releaseDocs.sh
+++ releaseDocs.sh
@@ -9,7 +9,7 @@ TEMP_REPO_DIR=$PWD/../docs-gh-pages
 ACTOR=$1
 TOKEN=$2
 
-remote_repo="https://${ACTOR}:${TOKEN}@github.com/sdl/multiterm-api-docs.git"
+remote_repo="https://${ACTOR}:${TOKEN}@github.com/rws/multiterm-api-docs.git"
 
 echo "Removing temporary doc directory $TEMP_REPO_DIR"
 rm -rf $TEMP_REPO_DIR
